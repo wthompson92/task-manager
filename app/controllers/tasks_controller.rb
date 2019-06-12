f201b2c54cb54d1b8bf896fe4e728df0f60cb26c
username <wnt925@gmail.com>
2019-06-11 22:47:08 -0600

    Add update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,4 +22,15 @@ class TasksController < ApplicationController
   def edit
   @task = Task.find(params[:id])
   end
+
+  def update
+  task = Task.find(params[:id])
+  task.update({
+    title: params[:task][:title],
+    description: params[:task][:description]
+    })
+  task.save
+  redirect_to "/tasks/#{task.id}"
+  end
+
 end
